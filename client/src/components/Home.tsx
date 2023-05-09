d731410509587a2e421b52c4398a9b4474061126
Jorge Castro <david92_jc@yahoo.es>
2023-05-09 17:45:14 -0500

    Creada página de inicio

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,6 +1,40 @@
+import { useEffect, useState } from "react"
+import { TPokemon } from "../api/config"
+import { getAllPokemon } from "../api/getAllPokemon"
+
 const Home = () => {
+    const [allPokemon, setAllPokemon] = useState<TPokemon[]>([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchAllPokemon = async () => {
+            const newPokemon = await getAllPokemon()
+            setAllPokemon(newPokemon)
+            setTimeout(() => {
+                setIsLoading(false)
+            }, 1000);
+        }
+        fetchAllPokemon()
+    }, [])
+
     return (
-        <div>Home</div>
+        <div>
+            {isLoading ? <p>Cargando...</p> : (
+                <div>
+                    <h1>Lista de Pokémon</h1>
+                    <ul className='decks'>
+                        {allPokemon.map(pokemon => (
+                            <li key={pokemon.id}>
+                                <img src={pokemon.img} alt="Imagen pokémon" />
+                                <h4>{pokemon.nombre}</h4>
+                                <p>{pokemon.descripcion}</p>
+                                <button>Más detalles</button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
     )
 }
 
